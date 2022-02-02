554698c64c158950e5994fa8389038544131926b
Mineru98 <rmstjr1030@naver.com>
2022-02-03 03:23:36 +0900

    Update Code

---
+++ src/components/Header.svelte
@@ -0,0 +1,22 @@
+<script>
+    let name = "world";
+    function assign() {
+        name = "test";
+    }
+    export let age = 21
+    export let world = "world"
+</script>
+
+<h1 class={age < 85 ? 'active' : ''}>{name}</h1>
+<button on:click={assign}>assign</button>
+<img src="" alt={world} />
+<input type="text" bind:value={name} />
+  
+<style>
+h1 {
+    color: red;
+}
+.active {
+    color: blue;
+}
+</style>
